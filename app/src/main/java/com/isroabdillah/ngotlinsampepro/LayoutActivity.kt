6177eb9b37d2e5a088d099d5739b9fe4a24f4a88
isroabdillah <isroabdillah.dev@gmail.com>
2023-04-21 22:29:16 +0700

    #3 view & layout

---
+++ app/src/main/java/com/isroabdillah/ngotlinsampepro/LayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.isroabdillah.ngotlinsampepro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout)
+    }
+}
